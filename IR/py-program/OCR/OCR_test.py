2779e11c29b7ba8488e3890bc68d2111d90726c9
m22023 <m22023@g.metro-cit.ac.jp>
2023-10-10 17:01:45 +0900

    detect square region around a letter(H,S,U) by limiting area.

--- IR/py-program/OCR/OCR_test.py
+++ IR/py-program/OCR/OCR_test.py
@@ -1,4 +1,4 @@
-import numpy as num
+import numpy as np
 import cv2 as cv
 import os
 
@@ -11,38 +11,62 @@ while True:
     want = input()
 
     if want == "H":
-        image = cv.imread('robocup_H.png')
+        image = cv.imread('realW_H.jpg')
         break
     elif want == "S":
-        image = cv.imread('robocup_S.png')
+        image = cv.imread('realW_S.jpg')
         break
     elif want == "U":
-        image = cv.imread('robocup_U.png')
+        image = cv.imread('realW_U.jpg')
         break
     else:
         print("select H or S or U")
 
 
+#Preprocess
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)    #convert BGR to GRAY
 
 blur = cv.GaussianBlur(gray, (5, 5), 0)         #Gaussian-filter
 
-thresh = cv.adaptiveThreshold(blur, 255, 1, 1, 91, 2)  #convert only 2 color(black or white) iamge
+#thresh = cv.adaptiveThreshold(blur, 255, 1, 1, 91, 2)  #convert only 2 color(black or white) iamge
+A, thresh = cv.threshold(blur, 75, 255, cv.THRESH_BINARY_INV)
 
 
 contours = cv.findContours(
     thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[0]
 
-for moji in contours:
-    x, y, w, h = cv.boundingRect(moji)
-    if h < 20: continue
+
+for i, countor in enumerate(contours):
+
+    area = cv.contourArea(countor, True)
+    if(area < 0):
+        area = area * -1
+
+    if area < 250000: continue         #if area is less than 500 pixel, Remove
+    
+    x, y, w, h = cv.boundingRect(countor)
     red = (0, 0, 255)
-    cv.rectangle(image, (x, y), (x+w, y+h), red, 2)
+    cv.rectangle(image, (x, y), (x+w, y+h), red, 10)
+
+    print(f"面積[{i}]: {area}")
 
 
-#cv.imwrite('re-moji.png', image)
+#for moji in contours:
+#    x, y, w, h = cv.boundingRect(moji)
+#    if h < 20: continue
+#    red = (0, 0, 255)
+#    cv.rectangle(image, (x, y), (x+w, y+h), red, 2)
 
 
-cv.imshow("image", thresh)
+
+cv.imwrite('re-moji.png', image)
+#print(image.shape)
+
+cv.namedWindow('out_image', cv.WINDOW_NORMAL)
+cv.namedWindow('in_image', cv.WINDOW_NORMAL)
+#height = image.shape[0]
+#width = image.shape[1]
+#Rimage = cv.resize(image, (width/2, height/2))
+cv.imshow('in_image', image)
+cv.imshow('out_image', thresh)
 cv.waitKey(0)
-
