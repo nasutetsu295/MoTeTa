315c4b86cd9d3640d61c1a041033e9e26212c14f
m22023 <m22023@g.metro-cit.ac.jp>
2023-11-11 16:36:53 +0900

    add program for 2 cameras

--- IR/py-program/raspi/sence_color.py
+++ IR/py-program/raspi/sence_color.py
@@ -8,7 +8,10 @@ from PIL import Image
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout = 1.0)
 
 
-cap = cv.VideoCapture(2)
+cap = cv.VideoCapture(0)         # if you know index, refer to " ls -la /dev/video* "
+cap.set(cv.CAP_PROP_FPS, 5)
+cap2 = cv.VideoCapture(1)
+cap2.set(cv.CAP_PROP_FPS, 5)
 
 
 
@@ -30,7 +33,7 @@ upper_green = (85, 255, 255)  # 緑色の上限(H, S, V)
 width = 300
 hight = 300
 
-blur_block = 3
+blur_block = 5
 
 avaliable = 0                       #avaliable == 1 --> there is letter in frame.
 
@@ -94,140 +97,157 @@ def mksquare(im, x, y, w, h):   #become 0 without inside of square  :  (image, X
 
 # (< loop >)-------------------------------------------------------------------------------------------------------------------------------------------
 while True:
-
-
-
-    ret, frame = cap.read()
     
-    if not ret:
-        break
+    iii = 1
     
-    
-
-    #(< Preprocess >)
-
-    change = Image.fromarray(np.uint8(frame))      #change small size of image
-    resize = np.asarray(change.resize((width, hight)))
-    
-    hsv_frame = cv.cvtColor(resize, cv.COLOR_BGR2HSV)           #convert BGR into HSV
-    
-    gray = cv.cvtColor(resize, cv.COLOR_BGR2GRAY)    #convert BGR into GRAY
-    blur = cv.blur(gray, (blur_block, blur_block))
-    A, thresh = cv.threshold(blur, TwoValue_thresh, 255, cv.THRESH_BINARY_INV)
-
-
-
-    #(< 色の判定を実行 >)
-    
-    R = check_color(hsv_frame, lower_red, upper_red, "Red")
-    Y = check_color(hsv_frame, lower_yellow, upper_yellow, "Yellow")
-    G = check_color(hsv_frame, lower_green, upper_green, "Green")
-    
-    
-    #comfirm the most color
-    
-    if (R > Y and R > G):   #Red is the most
-        mount = R
-        color = 1
-    elif (Y > G):           #yellow is the most
-        mount = Y
-        color = 2
-    else:                   #green is the most
-        mount = G
-        color = 3
-    
-    
-    
-    #(< recognize >)
+    while (iii <= 2):
         
-    contours = cv.findContours(
-        thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[0]
-
-    
-    avaliable = 0
-    for i, countor in enumerate(contours):
-
-        area = cv.contourArea(countor, True)
-        if(area < 0):
-            area = area * -1
-
-        if area < area_thresh: continue         #if area is less than area_thresh pixel, Remove
-        
-        avaliable = 1
-        x, y, w, h = cv.boundingRect(countor)
-        
-        red = (0, 0, 255)
-        cv.rectangle(resize, (x, y), (x+w, y+h), red, 2)
-    
-    
-    if avaliable == 1:
-
-        Top = thresh.copy()     # this is copy, not refer. a copy is different from origin (of memory).
-        Bottom = thresh.copy()
-
-
-        #width,hight of square(mksquare)
-        Top_W = w/3
-        Top_H = h/3
-        Bottom_W = w/3
-        Bottom_H = h/3
-
-
-        mksquare(Top, x+w/3, y, Top_W, Top_H)               # make mask of top region of the letter 
-        mksquare(Bottom,x+w/3, y+2*h/3, Bottom_W, Bottom_H) # make mask of bottom region of the letter
-
-        Top_rate = cv.countNonZero(Top) / (Top_W * Top_H) * 100                 # get area percentage of top region of the letter
-        Bottom_rate = cv.countNonZero(Bottom) / (Bottom_W * Bottom_H) * 100     # get area percentage of bottom region of the letter
-
-
-        if (Top_rate < Top_rate_thresh):            # if Top_rate is nothing, the picture is H or U. But, if not, the picture is U(inverted)x or S.
-
-            if(Bottom_rate < Bottom_rate_thresh):    # if Bottom rate is nothing, the picture is H. But , if not, the picture is U
-                letter = "H"
-    
-            else:
-                letter = "U"
-                
+        if (iii == 1):
+            ret, frame = cap.read()
         else:
-            
-            if (Bottom_rate < Bottom_rate_thresh):  # if Bottom rate is nothing, the picture is U(inverted). But , if not, the picture is S
-                letter = "U"
+            ret, frame = cap2.read()
+    
+        if not ret:
+            break
+    
+    
 
-            else:
-                letter = "S"
-            
+        #(< Preprocess >)
 
-    else:
-        letter = "none"
-        Top_rate = "none"
-        Bottom_rate = "none"
-
-
-
-    #If the most color is large, connect to M5
-    if (mount >= judge):
-        ser.write(color)
-        print("color : ", color)
+        change = Image.fromarray(np.uint8(frame))      #change small size of image
+        resize = np.asarray(change.resize((width, hight)))
         
-    else:
-        print("color : ", 0)
-        ser.write(0)              #neither the three color in camera
-    
-    
-    print("Top rate : ", Top_rate)
-    print("Bottom rate : ", Bottom_rate)
-    print(letter)
-    
-    cv.imshow("thresh_frame", thresh)
-    cv.imshow("bgr_frame", resize)
-    
-    key = cv.waitKey(2)
-    
-    # Escキーを入力されたら画面を閉じる
-    if key == 27:
-        break
+        hsv_frame = cv.cvtColor(resize, cv.COLOR_BGR2HSV)           #convert BGR into HSV
+        
+        gray = cv.cvtColor(resize, cv.COLOR_BGR2GRAY)    #convert BGR into GRAY
+        blur = cv.blur(gray, (blur_block, blur_block))
+        A, thresh = cv.threshold(blur, TwoValue_thresh, 255, cv.THRESH_BINARY_INV)
+
+
+
+        #(< 色の判定を実行 >)
+        
+        R = check_color(hsv_frame, lower_red, upper_red, "Red")
+        Y = check_color(hsv_frame, lower_yellow, upper_yellow, "Yellow")
+        G = check_color(hsv_frame, lower_green, upper_green, "Green")
+        
+        
+        #comfirm the most color
+        
+        if (R > Y and R > G):   #Red is the most
+            mount = R
+            color = 1
+        elif (Y > G):           #yellow is the most
+            mount = Y
+            color = 2
+        else:                   #green is the most
+            mount = G
+            color = 3
+        
+        
+        
+        #(< recognize >)
+            
+        contours = cv.findContours(
+            thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[0]
+
+        
+        avaliable = 0
+        for i, countor in enumerate(contours):
+
+            area = cv.contourArea(countor, True)
+            if(area < 0):
+                area = area * -1
+
+            if area < area_thresh: continue         #if area is less than area_thresh pixel, Remove
+            
+            avaliable = 1
+            x, y, w, h = cv.boundingRect(countor)
+            
+            red = (0, 0, 255)
+            cv.rectangle(resize, (x, y), (x+w, y+h), red, 2)
+        
+        
+        if avaliable == 1:
+
+            Top = thresh.copy()     # this is copy, not refer. a copy is different from origin (of memory).
+            Bottom = thresh.copy()
+            left = thresh.copy()
+            right = thresh.copy()
+
+            #width,hight of square(mksquare)
+            Top_W = w/3
+            Top_H = h/3
+            Bottom_W = w/3
+            Bottom_H = h/3
+
+            mksquare(Top, x+w/3, y, Top_W, Top_H)               # make mask of top region of the letter 
+            mksquare(Bottom,x+w/3, y+2*h/3, Bottom_W, Bottom_H) # make mask of bottom region of the letter
+            mksquare(left, x, y+h/3, Top_W, Top_H)               # make mask of top region of the letter 
+            mksquare(right,x+2*w/3, y+h/3, Bottom_W, Bottom_H) # make mask of bottom region of the letter
+
+
+            Top_rate = cv.countNonZero(Top) / (Top_W * Top_H) * 100                 # get area percentage of top region of the letter
+            Bottom_rate = cv.countNonZero(Bottom) / (Bottom_W * Bottom_H) * 100     # get area percentage of bottom region of the letter
+            left_rate = cv.countNonZero(left) / (Top_W * Top_H) * 100                 # get area percentage of top region of the letter
+            right_rate = cv.countNonZero(right) / (Bottom_W * Bottom_H) * 100     # get area percentage of bottom region of the letter
+
+
+            # the case of no trend of  a letter
+
+            if (Top_rate < Top_rate_thresh):            # if Top_rate is nothing, the picture is H or U. But, if not, the picture is U(inverted)x or S.
+
+                if(Bottom_rate < Bottom_rate_thresh):    # if Bottom rate is nothing, the picture is H. But , if not, the picture is U
+                    letter = "H"
+       
+                else:
+                    letter = "U"
+                    
+            else:
+                
+                if (Bottom_rate < Bottom_rate_thresh):  # if Bottom rate is nothing, the picture is U(inverted). But , if not, the picture is S
+                    letter = "U"
+
+                else:
+                    letter = "S"
+                
+
+        else:
+            letter = "none"
+            Top_rate = "none"
+            Bottom_rate = "none"
+
+
+
+        #If the most color is large, connect to M5
+        if (mount >= judge):
+            ser.write(color)
+            print("color : ", color)
+            
+        else:
+            print("color : ", 0)
+            ser.write(0)              #neither the three color in camera
+        
+        
+        print("Top rate : ", Top_rate)
+        print("Bottom rate : ", Bottom_rate)
+        print(letter)
+        
+        cv.imshow("thresh_frame", thresh)
+        cv.imshow("blur_frame", blur)
+        cv.imshow("bgr_frame", resize)
+        
+        key = cv.waitKey(2)
+        
+        # Escキーを入力されたら画面を閉じる
+        if key == 27:
+            break
+        
+        
+        iii = iii + 1
 
 cap.release()
+cap2.release()
 
 cv.destroyAllWindows()
 
